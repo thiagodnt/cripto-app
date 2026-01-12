9dcb290b9da20a714dc35c5dcce0f0b7bb464e55
thiagodnt <thiagodnt@hotmail.com>
2026-01-12 20:11:47 -0300

    feat: estrutura tabela home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,54 @@
+import styles from './home.module.css';
+import { BsSearch } from 'react-icons/bs';
+import { Link } from 'react-router';
+
 export function Home() {
 	return (
-		<div>
-			<h1>Home</h1>
-		</div>
+		<main className={styles.container}>
+			<form className={styles.form}>
+				<input
+					type="text"
+					placeholder="Digite aqui o nome da moeda (Ex: BitCoin)"
+				/>
+				<button type="submit">
+					<BsSearch size={30} color="#FFF" />
+				</button>
+			</form>
+
+			<table>
+				<thead>
+					<tr>
+						<th scope="col">Moeda</th>
+						<th scope="col">Valor Mercado</th>
+						<th scope="col">Preço</th>
+						<th scope="col">Volume</th>
+						<th scope="col">Mudança 24h</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr className={styles.tr}>
+						<td className={styles.td} data-label="Moeda">
+							<div className={styles.name}>
+								<Link to={'/detail/bitcoin'}>
+									<span>BitCoin</span> | BTC
+								</Link>
+							</div>
+						</td>
+						<td className={styles.td} data-label="Valor Mercado">
+							1T
+						</td>
+						<td className={styles.td} data-label="Preço">
+							8.000
+						</td>
+						<td className={styles.td} data-label="Volume">
+							2B
+						</td>
+						<td className={styles.td} data-label="Mudança 24h">
+							<span>1.20</span>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</main>
 	);
 }
